150f9657a7656dc13036a9862e04e576aa19f9c6
Rafal Szymanski <compate97@gail.com>
2021-05-08 23:27:16 +0200

    Initial commit

---
+++ src/main/java/com/shymmi/itscouts/ItscoutsApplication.java
@@ -0,0 +1,13 @@
+package com.shymmi.itscouts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItscoutsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItscoutsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shymmi/itscouts/ItscoutsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shymmi.itscouts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItscoutsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
